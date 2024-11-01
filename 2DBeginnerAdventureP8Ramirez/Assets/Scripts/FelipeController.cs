e5398dc4923c439aa4e75ea9c2e481e895be5500
Ry4nr <756238@student.dusd.net>
2024-11-01 15:27:05 -0700

    I added damage zones and a enemy
    I created a damage zone that when I walk into it, it does damage over time.  I also added a enemy/robot that walks back and fourth every 3 seconds.

--- 2DBeginnerAdventureP8Ramirez/Assets/Scripts/FelipeController.cs
+++ 2DBeginnerAdventureP8Ramirez/Assets/Scripts/FelipeController.cs
@@ -6,8 +6,12 @@ public class FelipeController : MonoBehaviour
 {
     public float speed = 9.0f;
     public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
     int currentHealth;
 
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -18,6 +22,7 @@ public class FelipeController : MonoBehaviour
       rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         
+        
     }
 
     // Update is called once per frame
@@ -25,6 +30,15 @@ public class FelipeController : MonoBehaviour
     {
          horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
+
+        if(isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
         
     }
      void FixedUpdate()
@@ -35,8 +49,18 @@ public class FelipeController : MonoBehaviour
         
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }   
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
