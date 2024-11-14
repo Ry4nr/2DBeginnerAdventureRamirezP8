77b8e17973212cb8997968834620a82738ce1449
Ry4nr <756238@student.dusd.net>
2024-11-14 15:27:06 -0800

    I added projectiles to my game
    My character is now able to shoot projectiles around the map.  I made this projectile a prefab.

--- 2DBeginnerAdventureP8Ramirez/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Ramirez/Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -10,6 +11,8 @@ public class EnemyController : MonoBehaviour
 
 
     Rigidbody2D rigidbody2d;
+
+    bool broken;
 
     float timer;
     int direction = 1;
@@ -26,6 +29,10 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!broken)
+        {
+            return;
+        }
         timer -= Time.deltaTime;
         if (timer < 0)
         {
@@ -35,6 +42,10 @@ direction = - direction;
     }
      void FixedUpdate()
     {
+        if (!broken)
+        {
+            return;
+        }
         Vector2 position = rigidbody2d.position;
         if (vertical)
         {
@@ -60,5 +71,11 @@ direction = - direction;
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2d.simulated = false;
     }
 }
--- 2DBeginnerAdventureP8Ramirez/Assets/Scripts/FelipeController.cs
+++ 2DBeginnerAdventureP8Ramirez/Assets/Scripts/FelipeController.cs
@@ -6,6 +6,9 @@ public class FelipeController : MonoBehaviour
 {
     public float speed = 9.0f;
     public int maxHealth = 5;
+
+    public GameObject projectilePrefab;
+
     public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
@@ -53,6 +56,10 @@ public class FelipeController : MonoBehaviour
                 isInvincible = false;
             }
         }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
         
     }
      void FixedUpdate()
@@ -79,5 +86,15 @@ public class FelipeController : MonoBehaviour
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+       Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
